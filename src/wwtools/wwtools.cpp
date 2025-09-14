11acbcad9126c242e59e76675a4442256a4048bf
Louis Poirier <contact@louispoirier.io>
2025-09-14 16:10:09 +0200

    build: finally use std::copy_n with missing include

--- src/wwtools/wwtools.cpp
+++ src/wwtools/wwtools.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <string>
 
 #include "revorb/revorb.hpp"
@@ -29,7 +30,7 @@ uint64_t get_wem_to_ogg_size(const uint8_t* indata, uint64_t insize) {
 void wem_to_ogg(const uint8_t *indata, uint64_t insize, uint8_t *outdata) {
   const auto wem = std::string(reinterpret_cast<const char*>(indata), insize);
   const std::string ogg = wem_to_ogg(wem);
-  std::memcpy(outdata, ogg.data(), ogg.size());
+  std::copy_n(reinterpret_cast<const uint8_t*>(ogg.data()), ogg.size(), outdata);
 }
 
 } // namespace wwtools
